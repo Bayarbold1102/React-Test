cd444aaf6970ba4c967b514f62a4f41f2eae000d
B170930013 <bayarbold61@gmail.com>
2021-10-26 19:06:39 +0800

    Flex

--- src/Views/Screen/Trading/Trading.js
+++ src/Views/Screen/Trading/Trading.js
@@ -5,17 +5,20 @@ import trading from '../../../Assets/css/trading.css'
 
 function Trading  () {
   return (
+    <div>
       <div className='container'>
-        <div className='chart' >
-          <div>
-          
+          <div className='first'>
+            <div className='chart' ></div>
+            <div className='container2'></div>
           </div>
-          </div>
-          <div className='info'>
-          </div>
-          <div className='info2'>
+          <div className='second'>
+            <div className='info'>
+            </div>
+            <div className='info2'>
+            </div>
           </div>
       </div>
+    </div>
   );
 };
  export default Trading;
