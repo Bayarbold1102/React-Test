9031ea109107d0a1dcf5b601f71310d5fb3ddafa
B170930013 <bayarbold61@gmail.com>
2021-10-27 20:28:50 +0800

    Flexy

--- src/Layout/TradeLayout.js
+++ src/Layout/TradeLayout.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {Link}  from 'react-router-dom';
 import { Layout, Menu } from 'antd';
 import { maintrade} from '../Assets/css/maintrade.css';
-import { Content } from 'antd/lib/layout/layout';
 
 const { Header} = Layout;
 
@@ -17,9 +16,8 @@ function TradeLayout({children}){
         <Menu.Item  className='profile'><Link to='/profile'>Boldko@gmail.com</Link></Menu.Item>
       </Menu>
     </Header>
-    <Content style={{ padding: '0 0px' }}>
       <div className="site-layout-content">{children}</div>
-    </Content>
+    
     
   </Layout>
   )
--- src/Views/Screen/Trading/Trading.js
+++ src/Views/Screen/Trading/Trading.js
@@ -1,24 +1,28 @@
 
 import React, { useState } from 'react';
 import trading from '../../../Assets/css/trading.css'
+import { Layout} from 'antd';
+import  TradeViewChart  from 'react-crypto-chart';
+import { Content } from 'antd/lib/layout/layout';
 
 
 function Trading  () {
   return (
-    <div>
-      <div className='container'>
-          <div className='first'>
-            <div className='chart' ></div>
-            <div className='container2'></div>
-          </div>
-          <div className='second'>
-            <div className='info'>
-            </div>
-            <div className='info2'>
-            </div>
-          </div>
-      </div>
-    </div>
+    <Layout>
+      <Layout className='container'>
+          <Layout className='first'>
+              <Content className='chart'></Content>
+            <Content className='info3'>
+              <Content className='info4'></Content>
+            </Content>
+          </Layout>
+          <Layout className='second'>
+            <Content className='info'>
+            </Content>
+            <Content className='info2'></Content>
+          </Layout>
+      </Layout>
+    </Layout>
   );
 };
  export default Trading;
