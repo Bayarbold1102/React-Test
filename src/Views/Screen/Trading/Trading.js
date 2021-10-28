a38a2452beeaf246c70f368b9a141abbf9fea405
B170930013 <bayarbold61@gmail.com>
2021-10-28 18:08:16 +0800

    Unset

--- src/Views/Screen/Trading/Trading.js
+++ src/Views/Screen/Trading/Trading.js
@@ -1,28 +1,32 @@
 
 import React, { useState } from 'react';
 import trading from '../../../Assets/css/trading.css'
-import { Layout} from 'antd';
+import { Layout, Typography} from 'antd';
 import  TradeViewChart  from 'react-crypto-chart';
 import { Content } from 'antd/lib/layout/layout';
 
+const {Text} = Typography;
 
 function Trading  () {
   return (
-    <Layout>
       <Layout className='container'>
           <Layout className='first'>
+            <Content className='header-chart'>
+              <Text className='header-chart-text' type='success'>Blooop</Text>
+            </Content>
               <Content className='chart'></Content>
             <Content className='info3'>
               <Content className='info4'></Content>
             </Content>
           </Layout>
-          <Layout className='second'>
-            <Content className='info'>
-            </Content>
-            <Content className='info2'></Content>
+          <Layout className='presecond'>
+            <Layout className='second'>
+              <Content className='info'></Content>
+              <Content className='info2'></Content>
+            </Layout>
+            <Content className='info5'></Content>
           </Layout>
       </Layout>
-    </Layout>
   );
 };
  export default Trading;
