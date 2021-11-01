2887c7fcb7a3bf5fed4e22454e3684a988356d84
B170930013 <bayarbold61@gmail.com>
2021-11-01 17:22:39 +0800

    Card

---
+++ src/Views/Auth/Components/Crypto.js
@@ -0,0 +1,18 @@
+import { Content } from "antd/lib/layout/layout";
+import React, {useState} from "react";
+import '../../../Assets/css/eth.css';
+import Bitcoin from "./Bitcoin";
+const Eth = () => {
+    const [button, setButton] = useState({btn1: true, btn2: true, btn3: true})
+
+    return(
+        <Content className='container' >
+            <Content  onClick={()=>setButton({...button, btn1: !button.btn1})} className={button.btn1 ? 'crypto1' : 'crypto1-1'}>
+                <Bitcoin/>
+            </Content>
+            <Content  onClick={()=>setButton({...button, btn2: !button.btn2})} className={button.btn2 ? 'crypto2' : 'crypto2-2'}></Content>
+            <Content  onClick={()=>setButton({...button, btn3: !button.btn3})} className={button.btn3 ? 'crypto3' : 'crypto3-3'}></Content>
+        </Content>
+    )
+}
+export default Eth;
--- src/Views/Screen/Trading/Trading.js
+++ src/Views/Screen/Trading/Trading.js
@@ -7,32 +7,24 @@ import  TradeViewChart  from 'react-crypto-chart';
 import { Content } from 'antd/lib/layout/layout';
 import Chart from '../../Auth/Components/Chart'
 
-import Bitcoin from '../../Auth/Components/Bitcoin'
+import Bitcoin from '../../Auth/Components/Bitcoin';
+import Crypto from 'Views/Auth/Components/Crypto';
 function Trading  () {
 
   return (
       <Layout className='container'>
           <Layout className='first'>
             <Content className='header-chart'>
-              <Content className='crypto1'>
-                <Bitcoin/>
-              </Content>
-              <Content className='crypto2'></Content>
-              <Content className='crypto3'></Content>
+             
+              <Crypto/>
             </Content>
               <Content className='chart'>
             <Chart/>
               </Content>
             <Content className='info3'>
-              <Content className='info4'></Content>
+              <Content className='info4'>
+              </Content>
             </Content>
-          </Layout>
-          <Layout className='presecond'>
-            <Layout className='second'>
-              <Content className='info'></Content>
-              <Content className='info2'></Content>
-            </Layout>
-            <Content className='info5'></Content>
           </Layout>
       </Layout>
   );
